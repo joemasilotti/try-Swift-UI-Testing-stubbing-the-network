df291940746afe414017fcbb6939ff324a1c747f
Joe Masilotti <joe@masilotti.com>
2020-05-17 16:22:20 -0400

    Stub network with swifter server

--- Board Game Shelf/Models/HTTPClient.swift
+++ Board Game Shelf/Models/HTTPClient.swift
@@ -3,8 +3,14 @@ import Foundation
 typealias HTTPCompletion = (Data?) -> Void
 
 struct HTTPClient {
-    private let baseURL = URL(string: "https://masilotti.com")!
     private let session = URLSession.shared
+    private let baseURL: URL = {
+        if let baseURL = ProcessInfo.processInfo.environment["base-url"] {
+            return URL(string: baseURL)!
+        } else {
+            return URL(string: "https://masilotti.com")!
+        }
+    }()
 
     func get(_ path: String, completion: HTTPCompletion?) {
         let url = baseURL.appendingPathComponent(path)
--- Board Game ShelfUITests/Board_Game_ShelfUITests.swift
+++ Board Game ShelfUITests/Board_Game_ShelfUITests.swift
@@ -1,10 +1,29 @@
 import XCTest
+import Swifter
 
 class Board_Game_ShelfUITests: XCTestCase {
     let app = XCUIApplication()
+    let server = HttpServer()
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+
+        try server.start()
+        server["/boardgames.json"] = { _ in
+            .ok(.json([
+                [
+                    "id": 265736,
+                    "name": "Tiny Towns",
+                    "minimumPlayers": 1,
+                    "maximumPlayers": 6,
+                    "imageName": "tiny-towns",
+                    "category": "Grid coverage",
+                    "description": "Game description."
+                ]
+            ]))
+        }
+        app.launchEnvironment["base-url"] = "http://localhost:8080"
+
         app.launch()
     }
 
